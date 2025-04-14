7c7d7380a989e773dd80bb3beb93130bbdd1a01f
JackoLaPatate <martignatnoam@gmail.com>
2025-04-14 08:52:49 +0200

    debut page accueil (oscour)

--- relicsoftimes/src/components/home.vue
+++ relicsoftimes/src/components/home.vue
@@ -4,8 +4,12 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 <template>
     <section id="home">
-        <h1 id="title">RELICS OF TIMES</h1>
-        <RouterLink to="/play" id="buttonTest">test du jeu</RouterLink>
+      <div class="container">
+          <h1 id="title">Relics Of Times</h1>
+          <img id="card1" src="../assets/cards1.png" alt="Cartes du jeu Relics Of Times" />
+      </div>
+
+<!--        <RouterLink to="/play" id="buttonTest">test du jeu</RouterLink>-->
         <!-- <a href="doYouWantToPlay.html"></a> -->
         <img id="flecheBasPageAcce" src="../assets/flecheBasPageAcce.svg"
             alt="Flèche indiquant un scrolle possible vers le bas">
@@ -17,18 +21,38 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <style>
     #home {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100vh;
-        width: 100vw;
-        background-color: #999494;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      width: 100vw;
+      background-color: #131313;
     }
+
+    .container {
+      display: flex;
+      flex-direction: row;
+      justify-content: flex-end;
+      align-items: center;
+      width: 100vw;
+    }
+
     #title {
-        font-size: 128px;
-        color: #fff;
+      position: absolute;
+      z-index: 99;
+      font-size: 96px;
+      color: #fff;
+      white-space: nowrap;
+      left: 5vw;
     }
+
+    #card1 {
+      right: 0;
+      height: 100%;
+      width: 100%;
+    }
+
     #buttonTest {
         margin-top: 20px;
         padding: 10px 20px;
@@ -45,7 +69,6 @@ import { RouterLink, RouterView } from 'vue-router'
         font-style: normal;
     }
     #flecheBasPageAcce{
-        margin-top: 20vh;
         width: 110px;
         height: 55px;
         
@@ -58,5 +81,16 @@ import { RouterLink, RouterView } from 'vue-router'
         height: 100vh;
         width: 100vw;
         background-color: #000000;
+    }
+
+    @media screen and (max-width: 1500px) {
+      .container {
+        justify-content: center;
+        align-items: center;
+      }
+      #card1 {
+        height: 100%;
+        width: 100%;
+      }
     }
 </style>
